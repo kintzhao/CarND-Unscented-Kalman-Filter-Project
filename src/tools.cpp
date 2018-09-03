5ddef3e669f0defff95b83141abeb9042f1d5acc
kintzhao <kint.zhao@slamtec.com>
2018-09-02 22:32:20 -0700

    init

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,15 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  for (size_t i = 0; i < estimations.size(); i++) {
+      VectorXd residual = estimations[i] - ground_truth[i];
+      // Coefficient-wise multiplication
+      residual = residual.array()*residual.array();
+      rmse += residual;
+  }
+  rmse /= estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
--- src/ukf.h
+++ src/ukf.h
@@ -102,6 +102,8 @@ public:
    * @param meas_package The measurement at k+1
    */
   void UpdateRadar(MeasurementPackage meas_package);
+
+  void UpdateUKF(MeasurementPackage meas_package, MatrixXd Zsig, int n_z);
 };
 
 #endif /* UKF_H */
